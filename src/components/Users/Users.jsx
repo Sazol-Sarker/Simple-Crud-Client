25eb596d39b49bb43af4083f4e68cff1355d6c93
Sazol Sarker <sazolsarker1@gmail.com>
2025-02-26 18:48:00 +0600

    DELETE api mongodb: delete single data

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,21 +1,52 @@
-import { useLoaderData } from 'react-router-dom';
-import Navbar from '../Navbar/Navbar';
+import { useLoaderData } from "react-router-dom";
+import Navbar from "../Navbar/Navbar";
+import { toast } from "react-toastify";
 
 const Users = () => {
-    const users=useLoaderData()
-    return (
-        <div>
-            <Navbar></Navbar>
-            <h2 className='text-teal-500 text-lg font-bold'>No of Users:{users.length}</h2>
-            <p className='text-teal-500 text-md font-bold italic'>Fetched from Mongodb Database via express server using GET READ API</p>
-            <ol className='border-2 border-teal-500 list-decimal w-1/2 mx-auto my-5 *:space-y-5'>
+//   receive server data
+    const users = useLoaderData();
 
-            {
-                users.map(usr=><li className='text-teal-500 text-lg font-semibold italic' key={usr._id}>{usr.name}</li>)
-            }
-            </ol>
-        </div>
-    );
+    //DELETE api
+  const handleDelete=(id)=>{
+    console.log("CLient handleDelete: id->",id);
+    fetch(`http://localhost:5000/users/${id}`,
+        {
+            method:'DELETE'
+        }
+    )
+    .then(res=>res.json())
+    .then(data=>{
+
+        console.log(data)
+        if(data.deletedCount==1)
+            toast('User deleted')
+    })
+
+}
+
+
+  return (
+    <div>
+      <Navbar></Navbar>
+      <h2 className="text-teal-500 text-lg font-bold">
+        No of Users:{users.length}
+      </h2>
+      <p className="text-teal-500 text-md font-bold italic">
+        Fetched from Mongodb Database via express server using GET READ API
+      </p>
+      <ol className="border-2 border-teal-500 list-disc w-1/2 mx-auto my-5 *:space-y-5">
+        {users.map((usr) => (
+          <li
+            className="text-teal-500 text-lg font-semibold italic flex justify-between border-2 border-blue-400"
+            key={usr._id}
+          >
+            {usr.name} {usr._id}
+            <button onClick={()=>handleDelete(usr._id)} className="btn">X</button>
+          </li>
+        ))}
+      </ol>
+    </div>
+  );
 };
 
 export default Users;
